08e08a223087e4c882a96d4c261115f3021ec864
Jasmol <jarora20@myseneca.ca>
2024-01-26 23:18:27 +0530

    Initial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// App.js
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Coding Maverick's Cloud Learning Hub</h1>
+        <p>Unlock the power of cloud technologies with our cutting-edge courses.</p>
+        <img src="cloud-icon.png" alt="Cloud Icon" className="cloud-icon" />
+        <button className="cta-button">Start Learning</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
